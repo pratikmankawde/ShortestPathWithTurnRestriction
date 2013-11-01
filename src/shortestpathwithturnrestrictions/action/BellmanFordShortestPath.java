f84da3db1dca02b37a2cebd35515849e266c2dc6
Pratik Mankawde <>
2013-11-01 11:16:44 +0530

    BellmanFord Shortest path implemented

--- src/shortestpathwithturnrestrictions/action/BellmanFordShortestPath.java
+++ src/shortestpathwithturnrestrictions/action/BellmanFordShortestPath.java
@@ -4,33 +4,64 @@
  */
 package shortestpathwithturnrestrictions.action;
 
+import java.util.ArrayList;
+import shortestpathwithturnrestrictions.model.Cost;
 import shortestpathwithturnrestrictions.model.GraphModel;
+import shortestpathwithturnrestrictions.utility.QueueImplementation;
 
 /**
  *
  * @author PprrATeekK
  */
 public class BellmanFordShortestPath {
-    
+
     GraphModel graph;
 
-    
-    public BellmanFordShortestPath(GraphModel graph){
-    
+    public BellmanFordShortestPath(GraphModel graph) {
+
         this.graph = graph;
+
     }
-    
-    
-    public void findShortestPath(int source, int destination){
-    
-        
-      /*   use this function to get adjacency matrix, store it in some variable or
-           use directly and calculate shortest path. Return it in the form of any data structure/object
-      */
-        graph.getAdjMat();
-        
-    
-    }   
-    
-    
+
+    public void findShortestPath(int source, int destination) {
+
+
+        /*   use this function to get adjacency matrix, store it in some variable or
+         use directly and calculate shortest path. Return it in the form of any data structure/object
+         */
+
+        Cost[][] matrix = graph.getAdjMat();
+        int n = graph.getNoOfNodes();
+        ArrayList<Integer> distance = new ArrayList<Integer>();
+        for (int j = 0; j < n; j++) {
+            Integer i = new Integer(Integer.MAX_VALUE);
+            distance.add(i);
+
+        }
+        QueueImplementation<Integer> que = new QueueImplementation<Integer>();
+        que.enqueue(source);
+        distance.set(source, 0);
+        while (que.size() != 0) {
+            Integer k = que.dequeue();
+            for (int x = 0; x < graph.getNoOfNodes(); x++) {
+                if (matrix[k][x].getFizedCost() != 0) {
+                    int dis = distance.get(k).intValue() + (int) matrix[k][x].getFizedCost();
+                    if (distance.get(x).intValue() > dis) {
+                        distance.set(x, dis);
+                    }
+                    int flag = 0;
+                    for (int l = 0; l < que.size(); l++) {
+                        // write code here to check if the object already exists in the queue or not
+                    }
+                    if (flag == 0) {
+                        que.enqueue(x);
+                    }
+
+
+                }
+            }
+
+        }
+
+    }
 }
---
+++ src/shortestpathwithturnrestrictions/utility/QueueImplementation.java
@@ -0,0 +1,88 @@
+package shortestpathwithturnrestrictions.utility;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class QueueImplementation<E extends Comparable<E>> implements QueueInterface<E> {
+	
+	// do not delete the definition of default constructor
+    private LinkedList<E> llque;
+	public QueueImplementation() {
+            llque = new LinkedList();
+	}
+	
+	@Override
+	public void enqueue(E e) {
+            try{
+            llque.offer(e);
+            }            
+            catch( IllegalArgumentException ex){
+                throw ex;
+            }
+	}
+
+	@Override
+	public E dequeue() {
+        try{
+	return llque.poll();
+            }
+            catch (NoSuchElementException ex){
+            throw ex;
+            }
+	}
+
+	@Override
+	public E peekMedian() {
+		 try{
+ 
+        
+        int median = (llque.size()/2)+1;
+ @SuppressWarnings("unchecked")
+ //        E[] eles=(E[])(Array.newInstance(getClass(),llque.size()));
+           
+        E[] eles =(E[])new Object[llque.size()];
+   
+        llque.toArray(eles);
+        Arrays.sort(eles);
+        return eles[median];
+            }
+            catch (java.util.NoSuchElementException ex){
+            throw ex;
+            }
+            catch (Exception ex){
+             ex.printStackTrace();
+             return null;
+            }
+           
+	}
+
+	@Override
+	public E peekMaximum()
+        {   E max=llque.getFirst();
+            for(E e : llque)
+            {
+            if(e.compareTo(max) >0)
+                max=e;
+            }
+		return max;
+	}
+
+	@Override
+	public E peekMinimum() {
+		E min=llque.getFirst();
+            for(E e : llque)
+            {
+            if(e.compareTo(min) <0)
+                min=e;
+            }
+		return min;
+	}
+
+	@Override
+	public int size() {
+		return llque.size();
+	}
+
+}
